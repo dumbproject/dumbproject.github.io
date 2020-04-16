39091273242f92781798bc59436c649a372d8194
zzivnuska <zzivnuska@gmail.com>
2020-04-16 06:42:21 -0700

    money even longer

--- javascript/money.js
+++ javascript/money.js
@@ -1,28 +1,37 @@
-var rate = 16.30 / 60 / 60
-
+var rate = 16.30
 var seconds = 0
 var profit = 0
 var x
-
-function count() {
-  stop()
-  x = setInterval(run, 100)
-}
+var minutes
+var hours
+var shift
 
 function run() {
-  seconds += 1
-  profit += rate
-  // console.log(profit)
+  stop()
+  x = setInterval(count, 100)
+}
 
+function count() {
+  seconds += .1
+  profit += (rate / 36000)
+  minutes = seconds / 60
+  hours = minutes / 60
+  shift = hours / 8
+  // console.log(profit)
   var paycheck = document.getElementById('paycheck')
-  paycheck.innerHTML = (seconds / 10) + ' seconds = $' + profit
+  paycheck.innerHTML = '<br>&nbsp;<span style="color:red;background-color:black;">' +
+                        Math.floor(seconds * 10) / 10 + ' seconds</span><br>&nbsp;' +
+                        Math.floor(minutes * 1000) / 1000 + ' minutes<br>&nbsp;' +
+                        Math.floor(hours * 100000) / 100000 + ' hours<br>&nbsp;' +
+                        Math.floor(shift * 1000000) / 1000000 + '% of 8hr shift<br><span style="background-color:white;color:green;">$' +
+                        Math.floor(profit * 100) / 100 + '</span>'
 }
 
 function stop() {
   if (x) {
     seconds = 0
     profit = 0
-    x.clearInterval()
+    // x.clearInterval()
     x = undefined
   }
 }
