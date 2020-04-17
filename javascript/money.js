e92784c5211778eb2c691c798a980800013147aa
zzivnuska <zzivnuska@gmail.com>
2020-04-16 21:30:49 -0700

    mostly style changes i think

--- javascript/money.js
+++ javascript/money.js
@@ -1,30 +1,38 @@
 var rate = 16.30
-var seconds = 0
+var ratemin = rate / 60
+var ratesec = rate / 60 / 60
+
 var profit = 0
 var x
+
+var seconds = 0
 var minutes
 var hours
 var shift
 
 function run() {
+  // document.getElementById("button").style.display = "none";
   stop()
-  x = setInterval(count, 100)
+  x = setInterval(count, 100) // every hundredth of a second
 }
 
 function count() {
-  seconds += .1
-  profit += (rate / 36000)
+  profit += (rate / 36000) // update
+  seconds += .1 // update the time by 1/10 of a second
   minutes = seconds / 60
   hours = minutes / 60
   shift = hours / 8
   // console.log(profit)
   var paycheck = document.getElementById('paycheck')
-  paycheck.innerHTML = '<br>&nbsp;<span style="color:red;background-color:black;">' +
+  paycheck.innerHTML = '<br><span style="background-color:green;color:white;">$' +
+                        Math.floor(profit * 100) / 100 + '</span><br><br>&nbsp;<span style="color:red;background-color:black;">' +
                         Math.floor(seconds * 10) / 10 + ' seconds</span><br>&nbsp;' +
                         Math.floor(minutes * 1000) / 1000 + ' minutes<br>&nbsp;' +
-                        Math.floor(hours * 100000) / 100000 + ' hours<br>&nbsp;' +
-                        Math.floor(shift * 1000000) / 1000000 + '% of 8hr shift<br><span style="background-color:white;color:green;">$' +
-                        Math.floor(profit * 100) / 100 + '</span>'
+                        Math.floor(hours * 100000) / 100000 + ' hours<br><br>&nbsp;' +
+                        Math.floor(shift * 1000000) / 1000000 + '% of 8hr shift<br><br>$' +
+                        ratesec.toFixed(6) + ' a second<br>$' +
+                        ratemin.toFixed(4) + ' a minute<br>$' +
+                        rate.toFixed(2) + ' an hour'
 }
 
 function stop() {
