a4af3bf5ccdbad1190eb3e01977a31d8273bd66a
zzivnuska <zzivnuska@gmail.com>
2020-08-29 18:05:44 -0700

    fixing money

--- javascript/money.js
+++ javascript/money.js
@@ -1,64 +1,49 @@
 var rate = 16.30
 var customRate = document.getElementById('rateInput')
-// console.log('rateInput', rateInput)
-
-var ratemin = rate / 60
-var ratesec = rate / 60 / 60
 
 var profit = 0
 var start
 
 var seconds = 0
-var minutes
-var hours
-var shift
-
 
 function handleButtonClick(event) {
-  // event.preventDefault()
-  console.log("rate: ", rate)
   run()
-  console.log(rate)
 }
 
-// customRate.addEventListener('change', handleButtonClick);
-
 function run() {
-  // var button = document.getElementById("button")
-  // button.classList.add("invisible")
   stop()
-  start = setInterval(count, 100) // every hundredth of a second
+  start = setInterval(count, 10) // every hundredth of a second
 }
 
 function count() {
   if (rate = !null) {
     rate = Number(customRate.value)
   }
-  profit = (customRate.value / 3600) * seconds // ate hourly wage per 1/0 of a second
-  seconds += .1 // update the time by 1/10 of a second
-  minutes = seconds / 60
-  hours = minutes / 60
-  shift = hours / 8
+  profit = (customRate.value / 3600) * seconds // update hourly wage per 1/100 of a second
+  seconds += .01 // update the time by 1/100 of a second
+  var minutes = seconds / 60
+  var hours = minutes / 60
+  var shift = hours / 8
 
-  var secondly = rate / 3600
-  var minutely = rate / 60
-  var day = rate * 8
+  var perSecond = rate / 3600
+  var perMinute = rate / 60
+  var perDay = rate * 8
 
   var inputvalue
   inputvalue = rate
   inputvalue = customRate
   var paycheck = document.getElementById('paycheck')
   paycheck.innerHTML = '<br><span style="background-color:green;color:white;">$' +
-                        Math.floor(profit * 100) / 100        + '</span><br><br>&nbsp;<span style="color:red;background-color:black;">' +
-                        seconds.toFixed(1)         + ' seconds</span><br>&nbsp;' +
+                        profit.toFixed(2) + '</span><br><br>&nbsp;<span style="color:red;background-color:black;">' +
+                        seconds.toFixed(2) + ' seconds</span><br>&nbsp;' +
                         minutes.toFixed(4) + ' minutes<br>&nbsp;' +
                         hours.toFixed(6) + ' hours<br><br>&nbsp;' +
-                        Math.floor(shift * 1000000) / 1000000 + '% of 8hr shift<br><br>$' +
+                        shift.toFixed(6) + '% of 8hr shift<br><br>$' +
 
-                        secondly.toFixed(6) + ' / second<br>$' +
-                        minutely.toFixed(4) + ' / minute<br>$' +
+                        perSecond.toFixed(6) + ' / second<br>$' +
+                        perMinute.toFixed(4) + ' / minute<br>$' +
                         rate.toFixed(2) + ' / hour<br>$' +
-                        day.toFixed(2) + ' / day<br><br>$' +
+                        perDay.toFixed(2) + ' / day<br><br>$' +
 
                         rate * 40 + ' / week<br>$' +
                         rate * 40 * 2 + ' / bi-weekly<br>$' +
