8480bdd9a8f56b4494107eb34153ef123d03d116
zzivnuska <zzivnuska@gmail.com>
2020-08-29 17:09:55 -0700

    fixin up home and money

--- javascript/money.js
+++ javascript/money.js
@@ -1,55 +1,65 @@
 var rate = 16.30
+var customRate = document.getElementById('rateInput')
+// console.log('rateInput', rateInput)
+
 var ratemin = rate / 60
 var ratesec = rate / 60 / 60
 
 var profit = 0
-var x
+var start
 
 var seconds = 0
 var minutes
 var hours
 var shift
-var day = rate * 8
 
-function handleChange(e) {
-  // e.preventDefault()
-  var inputRate = e.target.value
-  // console.log(e.target, inputRate)
+
+function handleButtonClick(event) {
+  // event.preventDefault()
+  console.log("rate: ", rate)
+  run()
+  console.log(rate)
 }
 
-var customRate = document.getElementById('rateinput')
-// console.log('rateInput', rateInput)
-customRate.addEventListener('change', (event) => {
-  // console.log(event.target.value)
-  rate = event.target.value
-});
+// customRate.addEventListener('change', handleButtonClick);
 
 function run() {
-  // document.getElementById("button").style.display = "none";
-  var button = document.getElementById("button")
-  button.classList.add("invisible")
+  // var button = document.getElementById("button")
+  // button.classList.add("invisible")
   stop()
-  x = setInterval(count, 100) // every hundredth of a second
+  start = setInterval(count, 100) // every hundredth of a second
 }
 
 function count() {
-  profit += (rate / 36000) // update
+  if (rate = !null) {
+    rate = Number(customRate.value)
+  }
+  profit = (customRate.value / 3600) * seconds // ate hourly wage per 1/0 of a second
   seconds += .1 // update the time by 1/10 of a second
   minutes = seconds / 60
   hours = minutes / 60
   shift = hours / 8
-  // console.log(profit)
+
+  var secondly = rate / 3600
+  var minutely = rate / 60
+  var day = rate * 8
+
+  var inputvalue
+  inputvalue = rate
+  inputvalue = customRate
   var paycheck = document.getElementById('paycheck')
   paycheck.innerHTML = '<br><span style="background-color:green;color:white;">$' +
-                        Math.floor(profit * 100) / 100 + '</span><br><br>&nbsp;<span style="color:red;background-color:black;">' +
-                        Math.floor(seconds * 10) / 10 + ' seconds</span><br>&nbsp;' +
-                        Math.floor(minutes * 1000) / 1000 + ' minutes<br>&nbsp;' +
-                        Math.floor(hours * 100000) / 100000 + ' hours<br><br>&nbsp;' +
+                        Math.floor(profit * 100) / 100        + '</span><br><br>&nbsp;<span style="color:red;background-color:black;">' +
+                        seconds.toFixed(1)         + ' seconds</span><br>&nbsp;' +
+                        minutes.toFixed(4) + ' minutes<br>&nbsp;' +
+                        hours.toFixed(6) + ' hours<br><br>&nbsp;' +
                         Math.floor(shift * 1000000) / 1000000 + '% of 8hr shift<br><br>$' +
-                        ratesec.toFixed(6) + ' / second<br>$' +
-                        ratemin.toFixed(4) + ' / minute<br>$' +
+
+                        secondly.toFixed(6) + ' / second<br>$' +
+                        minutely.toFixed(4) + ' / minute<br>$' +
                         rate.toFixed(2) + ' / hour<br>$' +
                         day.toFixed(2) + ' / day<br><br>$' +
+
                         rate * 40 + ' / week<br>$' +
                         rate * 40 * 2 + ' / bi-weekly<br>$' +
                         rate * 40 * 4 + ' / month<br>$' +
@@ -57,10 +67,10 @@ function count() {
 }
 
 function stop() {
-  if (x) {
+  if (start) {
     seconds = 0
     profit = 0
-    // x.clearInterval()
-    x = undefined
+    clearInterval(start)
+    start = undefined
   }
 }
